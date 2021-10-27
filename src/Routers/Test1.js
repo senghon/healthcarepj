9e2e75fb08496ad64bb0401c95906429f051fa42
senghon <ecovet91@naver.com>
2021-10-27 21:40:55 +0900

    css statrt

---
+++ src/Routers/Test1.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Test1() {
+  return (
+    <form>
+      <div>
+        <h1>동물병원명</h1>
+        <input value="hospitalName"></input>
+      </div>
+      <div>
+        <h1>차트번호</h1>
+        <input value="chartNum"></input>
+      </div>
+    </form>
+  );
+}
